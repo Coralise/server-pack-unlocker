4e495cc54b69a834ef71c0fdcef79e1a7d5a6990
Corals <waynegabule@gmail.com>
2022-03-20 08:08:02 +0800

    Code optimization

--- src/main/java/me/jaackson/spu/mixin/ClientBuiltinResourcePackProviderMixin.java
+++ src/main/java/me/jaackson/spu/mixin/ClientBuiltinResourcePackProviderMixin.java
@@ -1,23 +1,28 @@
 package me.jaackson.spu.mixin;
 
 import net.minecraft.client.resource.ClientBuiltinResourcePackProvider;
-import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourcePackCompatibility;
-import net.minecraft.resource.ResourcePackProfile;
-import net.minecraft.resource.ResourcePackSource;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.function.Supplier;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ClientBuiltinResourcePackProvider.class)
 public class ClientBuiltinResourcePackProviderMixin {
+
+	// target
+	// Lnet/minecraft/resource/ResourcePackProfile;<init>(
+	// 	Ljava/lang/String;
+	// 	Z
+	// 	Ljava/util/function/Supplier;
+	// 	Lnet/minecraft/text/Text;
+	// 	Lnet/minecraft/text/Text;
+	// 	Lnet/minecraft/resource/ResourcePackCompatibility;
+	// 	Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;
+	// 	Z
+	// 	Lnet/minecraft/resource/ResourcePackSource;)V
 	
-	@Redirect(at = @At("NEW"), method = "loadServerPack")
-	private ResourcePackProfile unlockServerPack(String name, boolean alwaysEnabled, Supplier<ResourcePack> packFactory, Text displayName, Text description, ResourcePackCompatibility compatibility, ResourcePackProfile.InsertionPosition direction, boolean pinned, ResourcePackSource source) {
-		return new ResourcePackProfile(name, alwaysEnabled, packFactory, displayName, description, compatibility, direction, false, source);
+	@ModifyArg(method = "loadServerPack", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;<init>(Ljava/lang/String;ZLjava/util/function/Supplier;Lnet/minecraft/text/Text;Lnet/minecraft/text/Text;Lnet/minecraft/resource/ResourcePackCompatibility;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)V"), index = 7)
+	public boolean falsify(boolean bool) {
+		return false;
 	}
 	
 }
